f4aaa3d65fe0cc70f958e4efd789a829a3031ae7
SKAT1005 <111747034+SKAT1005@users.noreply.github.com>
2024-01-29 09:03:25 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -5,3 +5,4 @@ class User(models.Model):
     unique_id = models.CharField(max_length=64, verbose_name='Уникальный Id пользователя')
     code = models.IntegerField(default=0, verbose_name='Код 2fa')
     last_message = models.IntegerField(default=0, verbose_name='Последнее сообщение пользователя')
+    attempt = models.IntegerField(default=0, verbose_name='Количество неудачных попыток')
